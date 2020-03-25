927df416279957845fd9e2112337d317bc8ea499
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-03-25 21:27:57 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,29 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]}"
+end
+
+def input_to_index(number)
+  value = "#{number}.to_i - 1"
+end
+
+def valid_move?(board, index)
+    index.between(0, 8) == true && position_taken?(board, index) == false
+end
+
+def position_taken?(board, index)
+  if board[index].strip != ""
+    board[index].strip != nil
+  else
+    false
+  end
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+end
