3d9987047aa6201dce3b31155770c906e7d135f4
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-03-25 21:46:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,4 +30,10 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if position_taken?(board, index) == false
+    move(board, index)
+
+
 end
